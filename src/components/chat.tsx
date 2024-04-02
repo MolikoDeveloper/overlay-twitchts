52bbe4002090cd453c299d1a69124a591c4508e7
albertosaurus_ac <molikodev@gmail.com>
2024-04-02 12:24:59 -0300

    Update Chat Styles.

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -1,43 +1,8 @@
 const Chat = () => {  
-  const css = /*CSS*/`
-  .chat{
-    overflow-y: auto;
-    height: 590px;
-    display: flex;
-    flex-direction: column-reverse;
-    font-size: 30px;
-    color: white;
-    font-weight: 1000px;
-    margin-left: 10px;
-    margin-right: 10px;
-    scrollbar-width: unset;
-    -ms-overflow-style: none;
-  }
-
-  .chat::-webkit-scrollbar {
-    display: none;
-  }
-
-  .newMessage{
-    display: block ruby;
-    margin-top: 10px;
-    overflow-wrap: pretty;
-    padding-bottom: 5px;
-  }
-  .user{
-    font-weight: 700;
-    text-shadow: 0 0 1px white, 0 0 1px black;
-  }
-  .message{
-    text-shadow: 0 0 3px #FF0000, 0 0 5px #0000FF;
-  }
-`;
-
   return (
     <>
-      <style>{css}</style>
-      <div class="chat">
-      </div>
+      <div class="chat"></div>
+      <link rel="stylesheet" href="/chat.css"/>
       <script type="module" src="/client.js"></script>
     </>
   )
