cff490cc44239daf1aa00f4ffc2c4eab5b06c03a
albertosaurus_ac <molikodev@gmail.com>
2024-04-02 12:09:59 -0300

    Removed express.

--- server.ts
+++ server.ts
@@ -1,66 +1,95 @@
-import express from "express";
-import ws from 'ws'
-import http from 'http';
-import path from "path";
-import fs from 'fs/promises';
+import http from "http";
+import ws from 'ws';
+import fs from 'fs';
 import renderToString from "preact-render-to-string";
-import App from "./src/App";
 import { type Options, IrcClient } from "twitchts";
+import App from "./src/App";
+
+const mimeTypes = {
+    '.js': 'text/javascript',
+    '.css': 'text/css',
+    '.json': 'application/json',
+    '.png': 'image/png',
+    '.jpg': 'image/jpg',
+    '.gif': 'image/gif',
+    '.svg': 'image/svg+xml',
+    '.wav': 'audio/wav',
+    '.mp4': 'video/mp4',
+    '.woff': 'application/font-woff',
+    '.ttf': 'application/font-ttf',
+    '.eot': 'application/vnd.ms-fontobject',
+    '.otf': 'application/font-otf',
+    '.wasm': 'application/wasm',
+    '.ico': 'image/x-icon'
+};
 
 const option: Options = {
-  identity: {
-    user: {
-      username: 'albertosaurus_ac',
-      token: process.env.token!,
-      refreshToken: ``,
-      code: ``
+    identity: {
+      user: {
+        username: 'albertosaurus_ac',
+        token: process.env.token!,
+        refreshToken: ``,
+        code: ``
+      },
+      app: {
+        clientId: process.env.clientid!,
+        secret: process.env.secret!,
+        redirect_uri: `https://albertoidesaurus.molikodev.com`,
+        events: ['ChannelFollow'],
+        actions: ['ModifyChannelInformation', 'SendChatMessage']
+      }
     },
-    app: {
-      clientId: process.env.clientid!,
-      secret: process.env.secret!,
-      redirect_uri: `https://albertoidesaurus.molikodev.com`,
-      events: ['ChannelFollow'],
-      actions: ['ModifyChannelInformation', 'SendChatMessage']
-    }
-  },
-  //channels: ["albertosaurus_ac", 'agustabell212', 'M2cG'],
-  debug: true,
-  testWebsocket: false
-}
+    //channels: ["albertosaurus_ac", 'agustabell212', 'M2cG'],
+    debug: true,
+    testWebsocket: false
+  }
 
-const http_service = http.createServer();
-const app = express();
-const wss = new ws.Server({ server: http_service, 'clientTracking': true })
+const server = http.createServer();
+const wss = new ws.Server({ server, clientTracking: true });
 const chat = new IrcClient(option);
 
-app.use(express.static(path.join(__dirname, 'public')));
+server.on('request', async (req, res) => {
+    //if (req.method != "POST") return res.end();
+    const _url = req.url || '';
+    const _ip = req.socket.remoteAddress;
+    const _mimes = Object.keys(mimeTypes);
+    let isStatic: boolean = false;
+    let channel = new URL(`http://0.0.0.0${req.url}`).searchParams.get('channel');
 
-app.use('*', async (req, res, next) => {
-  //const url = req.originalUrl.replace('/', '');
+    _mimes.forEach((d) => {
+        let _contentType = 'text/plain'
+        if (_url.endsWith(d)){
+            isStatic = true;
+            let _file = './wwwroot' + _url;
 
-  if (req.url.endsWith('.js')) {
-    res.setHeader('Content-Type', 'text/javascript');
-  }
-  next();
-})
+            fs.readFile(_file, (error, content) => {
+                if(!error) {
+                    // @ts-ignore
+                    _contentType = mimeTypes[d];
+                    res.writeHead(200, {'Content-Type': _contentType})
+                    res.end(content, 'utf-8')
+                }
+                else{
+                    res.writeHead(500);
+                    res.end('Error')
+                }
+            })
+        }
+    })
 
-app.get('*', async (req, res) => {
-  try {
-    if (req.url.includes('/?channel=')) {
-      let channel = new URL(`http://0.0.0.0${req.url}`).searchParams.get('channel');
-      if(channel){
-        setTimeout(() => {
-          chat.JoinChannel(channel!)
-        }, 3000)
-      }      
+    if(!isStatic){
+        res.setHeader('Content-Type', 'text/html');
+        res.end((await fs.promises.readFile('./index.html', 'utf-8')).replace('@code', renderToString(App({url: _url}))));
     }
-    const mainHTML = (await fs.readFile('./index.html', 'utf-8'));
 
-    res.send(mainHTML.replace('@code', renderToString(App({ url: req.url }))));
-  } catch (error) {
-    res.status(500).send('Internal Server Error');
-  }
+    if(channel){
+        setTimeout(() => {
+            chat.JoinChannel(channel!)
+            }, 3000)
+    }
+
 })
+
 wss.setMaxListeners(20);
 wss.on('connection', function connection(ws) {
   ws.on('close', () => {
@@ -71,15 +100,13 @@ wss.on('connection', function connection(ws) {
     if (ws.url.replace('/?channel=', '').toLocaleLowerCase() === channel.toLocaleLowerCase())
       if (!user.source?.isbot)
         ws.send(JSON.stringify({ user: user.tags?.["display-name"]!, message: message.replace(`\s+`, ''), color: user.tags?.color }))
-  })
+  });
 
   ws.on('message', () => {
     ws.close();
-  })
+  });
 });
 
-http_service.on('request', app)
-
-http_service.listen(8080, () => {
-  console.log(`Server is running http://localhost:8080`);
+server.listen(8080, () => {
+    console.log(`Server listens on http://localhost:8080`);
 });
