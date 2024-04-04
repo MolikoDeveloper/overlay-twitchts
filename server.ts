99e324256377c1fbbd0c854d5131497922dc6365
albertosaurus_ac <molikodev@gmail.com>
2024-04-04 16:50:28 -0300

    added Twitch Emotes and 7TV emotes support.

--- server.ts
+++ server.ts
@@ -1,114 +1,105 @@
-import http from "http";
-import ws from 'ws';
-import fs from 'fs';
-import renderToString from "preact-render-to-string";
-import { type Options, IrcClient } from "twitchts";
-import App from "./src/App";
+import { serve, file } from 'bun';
+import App from './src/App';
+import renderToString from 'preact-render-to-string';
+import { type Options, IrcClient } from 'twitchts';
 
-const mimeTypes = {
-    '.js': 'text/javascript',
-    '.css': 'text/css',
-    '.json': 'application/json',
-    '.png': 'image/png',
-    '.jpg': 'image/jpg',
-    '.gif': 'image/gif',
-    '.svg': 'image/svg+xml',
-    '.wav': 'audio/wav',
-    '.mp4': 'video/mp4',
-    '.woff': 'application/font-woff',
-    '.ttf': 'application/font-ttf',
-    '.eot': 'application/vnd.ms-fontobject',
-    '.otf': 'application/font-otf',
-    '.wasm': 'application/wasm',
-    '.ico': 'image/x-icon',
-    '.ogg': 'audio/ogg'
-};
-
+const NOTACCEPT = ["POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"]
 const option: Options = {
-    identity: {
-      user: {
-        username: 'albertosaurus_ac',
-        token: process.env.token!,
-        refreshToken: ``,
-        code: ``
-      },
-      app: {
-        clientId: process.env.clientid!,
-        secret: process.env.secret!,
-        redirect_uri: `https://albertoidesaurus.molikodev.com`,
-        events: ['ChannelFollow'],
-        actions: ['ModifyChannelInformation', 'SendChatMessage']
+  identity: {
+    user: {
+      username: 'albertosaurus_ac',
+      token: process.env.token!,
+      refreshToken: ``,
+      code: ``
+    },
+    app: {
+      clientId: process.env.clientid!,
+      secret: process.env.secret!,
+      redirect_uri: `https://albertoidesaurus.molikodev.com`,
+      events: ['ChannelFollow'],
+      actions: ['ModifyChannelInformation', 'SendChatMessage']
+    }
+  },
+  //channels: ["albertosaurus_ac", 'agustabell212', 'M2cG'],
+  debug: true,
+  testWebsocket: false
+}
+
+const chat = new IrcClient(option);
+chat.setMaxListeners(100);
+
+serve({
+  //development: true,
+  async fetch(req, server) {
+    const url = new URL(req.url)
+    let channel = url.searchParams.get('channel');
+    if (url.pathname.includes('..')) {
+      return;
+    }
+    // request to WebSocket
+    if (server.upgrade(req, { data: { channel: channel } })) {
+      return;
+    }
+
+    // GET
+    if (!NOTACCEPT.includes(req.method)) {
+      let isStatic: boolean = false;
+      let _file = __dirname + '/wwwroot' + url.pathname;
+
+      let staticfile = file(_file);
+      if (await staticfile.exists()) {
+        return new Response(staticfile);
+      }
+      // return webpage
+      else {
+        if (!url.pathname.startsWith('/api')) {
+          const app = (await file('./index.html').text()).replace('@code', renderToString(App({ url: url.pathname })));
+          return new Response(app, { status: 200, 'headers': { 'Content-Type': 'text/html' } });
+        }
+      }
+    }
+  },
+  websocket: {
+    sendPings: true,
+    idleTimeout: 10,
+    message(ws, message) {
+      if (message != "PONG")
+        ws.close();
+    },
+    close(ws) {
+      // @ts-ignore
+      const channel = ws.data?.channel;
+      if (channel) {
+
+        chat.LeaveChannel(channel)
+        chat.Close();
+        ws.close();
       }
     },
-    //channels: ["albertosaurus_ac", 'agustabell212', 'M2cG'],
-    debug: true,
-    testWebsocket: false
-  }
+    open(ws) {
+      // @ts-ignore
+      const channel: string = ws.data?.channel;
+      let _channelInfo: { name: string, id: string } = { name: '', id: '' };
 
-const server = http.createServer();
-const wss = new ws.Server({ server, clientTracking: true });
-const chat = new IrcClient(option);
+      setTimeout(() => {
+        chat.JoinChannel(channel!)
+      }, 3000)
 
-server.on('request', async (req, res) => {
-    //if (req.method != "POST") return res.end();
-    const _url = req.url || '';
-    const _ip = req.socket.remoteAddress;
-    const _mimes = Object.keys(mimeTypes);
-    let isStatic: boolean = false;
-    let channel = new URL(`http://0.0.0.0${req.url}`).searchParams.get('channel');
+      chat.on('userstate', (channel, userstate) => {
+        if (channel?.toLocaleLowerCase() === channel.toLocaleLowerCase())
+          _channelInfo = { name: channel, id: userstate["room-id"]! }
+      })
 
-    _mimes.forEach((d) => {
-        let _contentType = 'text/plain'
-        if (_url.endsWith(d)){
-            isStatic = true;
-            let _file = './wwwroot' + _url;
-
-            fs.readFile(_file, (error, content) => {
-                if(!error) {
-                    // @ts-ignore
-                    _contentType = mimeTypes[d];
-                    res.writeHead(200, {'Content-Type': _contentType})
-                    res.end(content, 'utf-8')
-                }
-                else{
-                    res.writeHead(500);
-                    res.end('Error')
-                }
-            })
-        }
-    })
-
-    if(!isStatic){
-        res.setHeader('Content-Type', 'text/html');
-        res.end((await fs.promises.readFile('./index.html', 'utf-8')).replace('@code', renderToString(App({url: _url}))));
-    }
-
-    if(channel){
-        setTimeout(() => {
-            chat.JoinChannel(channel!)
-            }, 3000)
-    }
-
-})
-
-wss.setMaxListeners(20);
-chat.setMaxListeners(20);
-wss.on('connection', function connection(ws) {
-  ws.on('close', () => {
-    chat.LeaveChannel(ws.url.replace('/?channel=', ''));  
-  })
-
-  chat.on('message', (channel, user, message, self) => {
-    if (ws.url.replace('/?channel=', '').toLocaleLowerCase() === channel.toLocaleLowerCase())
-      if (!user.source?.isbot)
-        ws.send(JSON.stringify({ user: user.tags?.["display-name"]!, message: message.replace(`\s+`, ''), color: user.tags?.color, emotes: user.tags?.emotes}))
-  });
-
-  ws.on('message', () => {
-    ws.close();
-  });
-});
-
-server.listen(8080, () => {
-    console.log(`Server listens on http://localhost:8080`);
+      chat.on('message', (channel, user, message, self) => {
+        ws.send(JSON.stringify({
+          channel: _channelInfo,
+          user: user.tags?.["display-name"]!,
+          message: message.replace(`\s+`, ''),
+          color: user.tags?.color,
+          emotes: user.tags?.emotes
+        }))
+      })
+    },
+  },
+  port: 8080
 });
--- wwwroot/client.js
+++ wwwroot/client.js
@@ -4,29 +4,81 @@ const timeParam = new URL(currentUrl).searchParams.get("time");
 const channelParam = new URL(currentUrl).searchParams.get("channel");
 const audioParam = new URL(currentUrl).searchParams.get("audio");
 
+
+let _7tvEmotes = [];
+let emoteList = {};
+let _7tvemoteList = {}
+
+let _7tvGlobalEmotes = fetch(`https://7tv.io/v3/emote-sets/global`).then(response => {
+    if (!response.ok) {
+        throw new Error('Network response was not ok');
+    }
+    return response.json();
+}).then(data => {
+    return data.emotes.map((emote) => {
+        return { name: emote.name, id: emote.id };
+    })
+})
+
+await _7tvGlobalEmotes.then(d => {
+    d.forEach(e => {
+        _7tvemoteList[e.name] = e.id;
+    })
+});
+
 if (channelParam) {
+    let socket = new WebSocket(`ws://localhost:8080?channel=${channelParam}`);
+
     const time = timeParam ? timeParam : 10;
-    globalThis.socket = new WebSocket(`ws://localhost:8080?channel=${channelParam}`);
 
     var messageBody = document.querySelector('.chat');
     messageBody.scrollTop = messageBody.scrollHeight - messageBody.clientHeight;
 
-    socket.addEventListener('open', function (event) {
-        console.log('WebSocket connection established');
-    });
+    const audio = document.createElement('audio');
+    audio.autoplay = false;
+    audio.controls = false;
+    audio.volume = audioParam;
 
-    socket.addEventListener('message', function (event) {
+    const audioSource = {
+        0: document.createElement('source'),
+        1: document.createElement('source'),
+        2: document.createElement('source'),
+        3: document.createElement('source'),
+        4: document.createElement('source')
+    }
+    for (let i = 0; i < 5; i++) {
+        audioSource[i].src = `/audio/${i}.ogg`
+        audioSource[i].type = 'audio/ogg';
+        audio.appendChild(audioSource[i]);
+    }
+
+    socket.addEventListener('message', async function (event) {
         const msg = JSON.parse(event.data);
+        if (_7tvEmotes.length === 0) {
+            _7tvEmotes = fetch(`https://7tv.io/v3/users/twitch/${msg.channel.id}`)
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok');
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    return data.emote_set.emotes.map((emote) => {
+                        return { name: emote.name, id: emote.id };
+                    });
+                })
+                .catch(error => {
+                    console.error('There was a problem getting the emote:', error);
+                });
+        }
 
         const chatDiv = document.querySelector('.chat');
 
         if (chatDiv) {
-            const audio = document.createElement('audio');
-            const audioSource = document.createElement('source');
             const newMessage = document.createElement('div');
             const user = document.createElement('span');
             const message = document.createElement('span');
-            let words = msg.message.split(' ');
+            const words = msg.message.split(' ');
             const fragment = document.createDocumentFragment();
 
             newMessage.className = 'newMessage'
@@ -36,45 +88,60 @@ if (channelParam) {
 
             if (audioParam) {
                 let file = Math.floor(Math.random() * 5)
-                audio.autoplay = true;
-                audio.controls = false;
-                audioSource.src = `/audio/${file}.ogg`
-                audioSource.type = 'audio/ogg';
-                audio.volume = audioParam;
-                audio.appendChild(audioSource);
-                newMessage.appendChild(audio);
+                let _audio = document.createElement('audio');
+                let _audiosource = audio.querySelectorAll('source')[file];
+                console.log(file, _audiosource);
+                if (_audiosource) {
+                    _audio.controls = false;
+                    _audio.autoplay = true;
+
+                    _audio.appendChild(_audiosource);
+                    newMessage.appendChild(_audio.cloneNode(true));
+                }
             }
 
-            let emoteList = {};
             if (msg.emotes) {
-
                 Object.keys(msg.emotes).forEach(d => {
                     msg.emotes[d].forEach(e => {
-                        let emo =  msg.message.slice(Number(e.startPosition), Number(e.endPosition)+1);
-                        if(!emoteList.hasOwnProperty(emo.trim())){
+                        let emo = msg.message.slice(Number(e.startPosition), Number(e.endPosition) + 1);
+                        if (!emoteList.hasOwnProperty(emo.trim())) {
                             emoteList[emo.trim()] = d;
                         }
                     })
                 });
             }
 
-            words.forEach(word => {
-                console.log(word)
-                if(emoteList.hasOwnProperty(word)){
-                    console.log(emoteList[word]);
+            await _7tvEmotes.then(d => {
+                d.forEach(e => {
+                    if(!_7tvemoteList[e.name])
+                        _7tvemoteList[e.name] = e.id;
+                })
+            });
+
+            for (let word of words) {
+                if (emoteList.hasOwnProperty(word)) {
                     const img = document.createElement('img');
                     img.className = 'emote';
                     img.src = `https://static-cdn.jtvnw.net/emoticons/v2/${emoteList[word]}/default/dark/1.0`;
+                    img.loading = 'lazy';
                     img.alt = word;
-                    fragment.appendChild(img)
+                    fragment.appendChild(img);
                 }
-                else{
-                    console.log(word);
+                else if (_7tvemoteList.hasOwnProperty(word)) {
+                    const img = document.createElement('img');
+                    const src = `https://cdn.7tv.app/emote/${_7tvemoteList[word]}/1x.avif`;
+                    img.className = 'emote';
+                    img.src = src;
+                    img.alt = word;
+                    fragment.appendChild(img);
+
+                }
+                else {
                     const span = document.createElement('span');
                     span.textContent = `${word} `;
                     fragment.appendChild(span);
                 }
-            })
+            }
 
             user.textContent = msg.user + ': ';
             //message.textContent = `: ${msg.message}`;
@@ -93,7 +160,6 @@ if (channelParam) {
     });
 
     socket.addEventListener('close', function (event) {
-        console.log('WebSocket connection closed');
         sleep(2000).then(() => { location.reload(); });
     });
 
